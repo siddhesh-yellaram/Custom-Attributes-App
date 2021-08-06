bb54b7c1a6d644a795b9d6331b45db7e47683af6
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 00:59:01 +0530

    Add files via upload

---
+++ CustomAttributesApp/Model/MyAttributes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAttributesApp.Model
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class Refactor : System.Attribute
+    { }
+    
+    [Refactor]
+    public class MyAttributes
+    {
+        [Refactor]
+        public static void Fug()
+        {
+            Console.WriteLine("Inside Fug");
+        }
+
+        [Refactor]
+        public static void Tower()
+        {
+            Console.WriteLine("Inside Tower");
+        }
+
+        [Refactor]
+        public static void Fruit()
+        {
+            Console.WriteLine("Inside Fruit");
+        }
+
+        [Refactor]
+        public static void Animal()
+        {
+            Console.WriteLine("Inside Animal");
+        }
+
+        [Refactor]
+        public static void Game()
+        {
+            Console.WriteLine("Inside Game");
+        }
+    }
+}
---
+++ CustomAttributesApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using CustomAttributesApp.Model;
+
+namespace CustomAttributesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type info = typeof(MyAttributes);
+            MemberInfo[] mi = info.GetMembers();
+            int count = 0;
+            for(int i = 0; i < mi.Length; i++)
+            {
+                Object[] attrs = mi[i].GetCustomAttributes(true);
+                for(int j = 0; j < attrs.Length; j++)
+                {
+                    if (attrs[j].ToString() == "CustomAttributesApp.Model.Refactor")
+                    {
+                        Console.WriteLine("\nThe attributes of the member"+mi[i]+" are "+attrs[j]);
+                        count += 1;
+                    }
+                }
+            }
+            Console.WriteLine("\nThe count for Custom Attribute is " + count);
+            Console.ReadLine();
+        }
+    }
+}
